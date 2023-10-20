c01edcbf120d5e0692e786f3f9b5406b847bdb2b
Hristo Koev <iamhristokoev@gmail.com>
2023-10-20 03:31:47 +0200

    uncomment

--- lib/revalidate.ts
+++ lib/revalidate.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export async function revalidate(tag: string) {
-  // axios.post(`https://hlinoteka.cz/api/revalidate?tag=${tag}&secret=b6zJ7huYMLM`)
+  axios.post(`https://hlinoteka.cz/api/revalidate?tag=${tag}&secret=b6zJ7huYMLM`)
 }
